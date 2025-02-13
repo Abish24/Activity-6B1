adab0389289c9573b46cb16d769918006317614d
Abisherk <Abiconsan24@gmail.com>
2025-02-13 16:00:52 -0500

    Created helloWorld Function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 class Main {
+    public static void HelloWorld() {
+        System.out.println("Hello World");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
